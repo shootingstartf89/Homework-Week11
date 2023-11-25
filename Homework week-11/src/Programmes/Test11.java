86b062714142427435835125a593530742df2b74
shootingstartf89 <nanavati.pankti@gmail.com>
2023-11-25 18:00:23 +0000

    Pushing the codes

---
+++ Homework week-11/src/Programmes/Test11.java
@@ -0,0 +1,15 @@
+/* Declare global variables of your name, Add all your group names in list, using
+for each loop iterate and print ONLY your name.*/
+
+package Programmes;
+
+public class Test11 {
+
+String Pankti;
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
